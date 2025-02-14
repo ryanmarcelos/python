28fc4b3f492427eeef51c91eba02b2d8cd74c294
RYAN <126591328+ryanmarcelos@users.noreply.github.com>
2025-02-14 12:16:08 -0300

    Add files via upload

---
+++ python/app.py
@@ -0,0 +1,18 @@
+print('Bar da Lora\n')
+
+print('1. Cadastrar restaurante')
+print('2. Listar restaurante')
+print('3. Ativar restaurante')
+print('4. Sair\n')
+
+opcao_escolhida = int(input('escolha uma opção: '))
+print(f'você escolheu a opção {opcao_escolhida}')
+
+if opcao_escolhida == 1:
+    print('Cadastrar restaurante')
+elif opcao_escolhida == 2:
+    print('Listar restaurante')
+elif opcao_escolhida == 3:
+    print('Ativar restaurante')
+else:
+    print('Encerrando o programa')
